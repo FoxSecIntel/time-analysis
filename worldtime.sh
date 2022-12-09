fdd6ccdce802190df484fb4baac2fd52a156862c
13gbc <118615125+13gbc@users.noreply.github.com>
2022-12-09 22:41:32 +0100

    Update worldtime.sh

--- worldtime.sh
+++ worldtime.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 # Set the time zone of San Francisco
-export TZ=America/Los_Angeles
-echo "The time in San Francisco is: $(date)"
+export TZ=US/Pacific
+echo -e "US Pacific: \t$(date)"
+
+export TZ=US/Central
+echo -e "US Central: \t$(date)"
 
 # Set the time zone of New York City
-export TZ=America/New_York
-echo "The time in New York City is: $(date)"
+export TZ=US/Eastern
+echo -e "US Eastern: \t$(date)"
 
 # Set the time zone of Amsterdam
 export TZ=Europe/Amsterdam
-echo "The time in Amsterdam is: $(date)"
+echo -e "Amsterdam: \t$(date)"
 
 # Set the time zone of Venice
 export TZ=Europe/Rome
-echo "The time in Venice is: $(date)"
+echo -e "Venice: \t$(date)"
 
 # Set the time zone of Hong Kong
 export TZ=Asia/Hong_Kong
-echo "The time in Hong Kong is: $(date)"
+echo -e "Hong Kong: \t$(date)"
