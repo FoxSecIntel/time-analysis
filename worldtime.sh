3060f55b7f9d754af80e59eadcd93b32d3a12619
13gbc <118615125+13gbc@users.noreply.github.com>
2022-12-10 12:44:21 +0100

    Update worldtime.sh

--- worldtime.sh
+++ worldtime.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-# Set the time zone of San Francisco
-export TZ=US/Pacific
-echo -e "US Pacific: \t$(date)"
+# Set the time zones to query
+time_zones=(US/Pacific US/Central US/Eastern Europe/Amsterdam Europe/Rome Asia/Hong_Kong)
 
-export TZ=US/Central
-echo -e "US Central: \t$(date)"
-
-# Set the time zone of New York City
-export TZ=US/Eastern
-echo -e "US Eastern: \t$(date)"
-
-# Set the time zone of Amsterdam
-export TZ=Europe/Amsterdam
-echo -e "Amsterdam: \t$(date)"
-
-# Set the time zone of Venice
-export TZ=Europe/Rome
-echo -e "Venice: \t$(date)"
-
-# Set the time zone of Hong Kong
-export TZ=Asia/Hong_Kong
-echo -e "Hong Kong: \t$(date)"
+# Loop over the time zones and print the current time for each one
+for tz in "${time_zones[@]}"; do
+  export TZ=$tz
+  printf "%-12s %s\n" "$tz:" "$(date)"
+done
