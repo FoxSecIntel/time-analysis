c6f79bd89c2a38253f0195383cf65c94b48d1e19
13gbc <118615125+13gbc@users.noreply.github.com>
2023-02-19 16:22:26 +0100

    Update get-yourage.sh

--- get-yourage.sh
+++ get-yourage.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-read -p "Enter your date of birth (YYYY-MM-DD): " dob
+# Check if the user provided the date of birth as an argument
+m=wqhWaWN0b3J5IGlzIG5vdCB3aW5uaW5nIGZvciBvdXJzZWx2ZXMsIGJ1dCBmb3Igb3RoZXJzLiAtIFRoZSBNYW5kYWxvcmlhbsKoCg==
+if [[ $# -eq 1 ]]; then
+    if [[ $1 == "m" ]]; then
+        echo $m | base64 --decode
+        exit 0
+    else
+        dob=$1
+    fi
+else
+    read -p "Enter your date of birth (YYYY-MM-DD): " dob
+fi
+
+# Check if the input is in date format
+if ! date -d "$dob" >/dev/null 2>&1; then
+    echo "Invalid date format. Please enter the date in YYYY-MM-DD format."
+    exit 1
+fi
 
 # Convert the date of birth to a Unix timestamp
 dob_timestamp=$(date -d "$dob" +%s)
